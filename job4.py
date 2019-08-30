6704a043c514ef4b5e38233166dcf108ec68b6a9
lixiaomin <lixiaomin@caikr.com>
2019-08-30 17:12:47 +0800

    简化job4的代码

--- job4.py
+++ job4.py
@@ -7,16 +7,13 @@
 #    高于32：严重肥胖
 #用if-elif判断并打印结果：
 age=input('请输入您的年龄:')
-#height = 1.75
-#weight = 80.5
 height=float(input('请输入您的身高(m)：'))
 weight=float(input('请输入您的体重(kg)：'))
 age=int(age)
-if age >= 18: 
+if age >= 18: #判断是否是成年人
     print('您的年龄是：',age,'adult')
 else:
     print('您的年龄是：',age,'teenager')
-#判断是否是成年人	
 BMI=weight/(height*height)  #计算BMI值
 BMI=float('%.2f' % BMI) #将BMI值转换成浮点数，且小数点后两位小数
 if BMI<18.5:
